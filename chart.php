7a0dec0812dc5fd66738287b2014070414306249
ghbtech-dev <55549505+ghbtech-dev@users.noreply.github.com>
2024-04-07 21:58:28 -0300

    Update chart.php
    Signed-off-by: ghbtech-dev <55549505+ghbtech-dev@users.noreply.github.com>

--- chart.php
+++ chart.php
@@ -1,1 +1,29 @@
+<?php
+// Verifica se os dados foram submetidos via POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+    $glucose = $_POST['glucose'];
+
+    // Abre o arquivo CSV para escrita
+    $file = fopen('data/glicemia.csv', 'a');
+    fputcsv($file, array($date, $time, $glucose));
+    fclose($file);
+}
+
+// Leitura dos dados do arquivo CSV
+$csvFile = file('data/glicemia.csv');
+$data = [];
+foreach ($csvFile as $line) {
+    $data[] = str_getcsv($line);
+}
+
+// Converte datas para o formato JavaScript
+$dates = [];
+foreach ($data as $item) {
+    $dates[] = date('Y-m-d H:i', strtotime($item[0] . ' ' . $item[1]));
+}
+
+// Saída dos dados em formato JSON
+echo json_encode($dates);
 
